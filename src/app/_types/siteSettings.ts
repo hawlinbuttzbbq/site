99dfc318fa67aa0ab10fc35e653a48361e8d48ad
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-16 15:56:11 -0400

    updated object sturcture

---
+++ src/app/_types/siteSettings.ts
@@ -0,0 +1,38 @@
+export type SiteSettings = {
+  address2: string;
+  favicon: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+  socialX: string;
+  _createdAt: string;
+  description: string;
+  address4: string;
+  _updatedAt: string;
+  address1: string;
+  _rev: string;
+  socialYoutube: string;
+  socialFacebook: string;
+  socialInstagram: string;
+  _id: string;
+  logoWeb: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+  address3: string;
+  logoMobile: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+  title: string;
+  chatWidget: string;
+};
