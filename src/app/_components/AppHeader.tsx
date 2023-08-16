f1891996f8275a927e5f04ad25ecbcefd537c147
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-16 17:00:33 -0400

    Added dynamic site information

--- src/app/_components/AppHeader.tsx
+++ src/app/_components/AppHeader.tsx
@@ -1,24 +1,35 @@
 import { MessageCircle } from "lucide-react";
 import React from "react";
 import ContactPopover from "./ContactPopover";
+import { SiteSettingsType } from "../_types/siteSettings";
+import { urlForImage } from "../_lib/sanity";
 
-const AppHeader: React.FC = () => {
+interface AppHeaderType {
+  data: SiteSettingsType;
+}
+
+const AppHeader = (props: AppHeaderType) => {
   return (
     <header className="bg-[#212529] border-b border-white sm:h-[77px] lg:h-[91px]">
       <div className="container mx-auto px-10 flex justify-center items-center h-full lg:justify-between max-w-none">
         <div className="flex items-center">
           <img
-            src="https://placehold.co/190x57"
+            src={urlForImage(props.data.logoWeb.asset._ref).url()}
             alt="Logo"
-            className="sm:w-[166px] sm:h-[50px] lg:w-[190px] lg:h-[57px]"
+            className="hidden lg:flex w-[190px] lg:h-[57px]"
+          />
+          <img
+            src={urlForImage(props.data.logoMobile.asset._ref).url()}
+            alt="Logo"
+            className="sm:w-[166px] sm:h-[50px] lg:hidden"
           />
         </div>
         <div className="lg:flex items-center space-x-8">
           <div className="hidden lg:flex flex-col font-medium text-white font-montserrat">
-            <span style={{ fontSize: "12px" }}>1503 Kelly's Landing Dr.</span>
-            <span style={{ fontSize: "12px" }}>Mount Holly NC 28120</span>
-            <span style={{ fontSize: "12px" }}>(704) 622-1772</span>
-            <span style={{ fontSize: "12px" }}>LMatthews2131@gmail.com</span>
+            <span style={{ fontSize: "12px" }}>{props.data.address1}</span>
+            <span style={{ fontSize: "12px" }}>{props.data.address2}</span>
+            <span style={{ fontSize: "12px" }}>{props.data.address3}</span>
+            <span style={{ fontSize: "12px" }}>{props.data.address4}</span>
           </div>
           <div className="hidden lg:flex border-l h-[48px]"></div>
           <MessageCircle className="hidden lg:flex text-white h-8 w-8" />
