bf0177fbfa91221e5d803707532367b5022c45cb
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-17 03:03:37 -0400

    Fixed share button

--- src/app/_components/ShareLinkBtn.tsx
+++ src/app/_components/ShareLinkBtn.tsx
@@ -2,6 +2,11 @@
 import React from "react";
 import { Share } from "lucide-react";
 import { usePathname } from "next/navigation";
+import Snackbar from "@mui/material/Snackbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { X } from "lucide-react";
+import { CONFIG } from "@/constains";
 
 interface ShareLinkBtnProps {
   className?: string;
@@ -9,11 +14,51 @@ interface ShareLinkBtnProps {
 
 const ShareLinkBtn = (props: ShareLinkBtnProps) => {
   const pathname = usePathname();
-  const handleSharePress = () => {
-    console.log(pathname);
+  const [open, setOpen] = React.useState(false);
+
+  const handleSharePress = async () => {
+    await navigator.clipboard.writeText(`${CONFIG.baseUrl}${pathname}`);
+
+    // Show the snackbar
+    setOpen(true);
   };
 
-  return <Share className={props.className} onClick={handleSharePress} />;
+  const handleDismiss = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleDismiss}
+      >
+        <X />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <>
+      <Share className={props.className} onClick={handleSharePress} />
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleDismiss}
+        message="Url copied successful!"
+        action={action}
+      />
+    </>
+  );
 };
 
 export default ShareLinkBtn;
