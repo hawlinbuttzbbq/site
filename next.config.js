5c3eeec2f291fe18da68c93076b46f1029cde29f
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-17 03:23:30 -0400

    fixed output back to export

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // Comment this out when using hosting that does not require static pages
-
-  async redirects() {
-    return [
-      {
-        source: "/admin-page",
-        destination: "https://hawlinbuttzbbq.sanity.studio",
-        permanent: true,
-      },
-    ];
-  },
+  output: "export", // Comment this out when using hosting that does not require static pages
 };
 
 module.exports = nextConfig;
