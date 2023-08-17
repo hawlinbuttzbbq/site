8d13894e6532fb0383cef81101678fe57e80fd08
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-17 03:18:30 -0400

    Added redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Comment this out when using hosting that does not require static pages
+  // output: "export", // Comment this out when using hosting that does not require static pages
+
+  async redirects() {
+    return [
+      {
+        source: "/admin-page",
+        destination: "https://hawlinbuttzbbq.sanity.studio",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
