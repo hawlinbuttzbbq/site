d0aba02522163e2a65576d03c1b91694a453c6ba
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-08-17 00:17:49 -0400

    test base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/site",
   output: "export", // Comment this out when using hosting that does not require static pages
 };
 
