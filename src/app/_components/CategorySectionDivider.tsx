5e4e8507b5df2cd109a63cfa5f9cf541c1fa7013
hawlinbuttzbbq <LMatthews2131@gmail.com>
2023-09-05 21:22:09 -0400

    added some comment notes

--- src/app/_components/CategorySectionDivider.tsx
+++ src/app/_components/CategorySectionDivider.tsx
@@ -3,13 +3,14 @@ interface SectionDividerProps {
   title: string;
 }
 
+// TODO: need to fix scroll to section. it scrolls to the middle of a section. not sure how to fix yet. but look into using offsets with nextjs
 const CategorySectionDivider = (props: SectionDividerProps) => {
   return (
-    <div id={props.divId.replace(/\s/g, "")} className="scroll-smooth p-10">
+    <section id={props.divId.replace(/\s/g, "")} className="scroll-smooth p-10">
       <h1 className="text-center font-bold text-2xl md:text-4xl">
         {props.title}
       </h1>
-    </div>
+    </section>
   );
 };
 
